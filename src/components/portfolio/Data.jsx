328a3c354cff3f5d550f34f5d0631deda6db0225
rsteyker <rmarchino@autonoma.edu.pe>
2025-05-22 00:21:12 -0500

    Updated info

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -7,25 +7,34 @@ import image6 from "../../assets/portfolio/meeti.jpg";
 import image7 from "../../assets/portfolio/planificar-gastos.jpg";
 import image8 from "../../assets/portfolio/pokedex.jpg";
 import image9 from "../../assets/portfolio/sound-music.jpg";
-
+import quiosco from "../../assets/portfolio/quiosco.png";
+import digito from "../../assets/portfolio/digito.png";
+import guitarra from "../../assets/portfolio/guitarra.png";
 
 export const Data = [
     {
         id: 1,
-        title: 'Quiosco Backend',
-        image: '',
+        title: 'Quiosco Frontend && Backend',
+        image: quiosco,
         codeLink: "https://github.com/rmarchino/quiosco-backend.git",
-        previewLink: ""
+        previewLink: "https://github.com/rmarchino/quiosco-frontend.git"
     },
     {
         id: 2,
-        title: 'Quiosco Frontend',
-        image: '',
-        codeLink: "https://github.com/rmarchino/quiosco-frontend.git",
-        previewLink: ""
+        title: 'CRM Dígito Backend && Frontend',
+        image: digito,
+        codeLink: "https://github.com/rmarchino/digital-backend.git",
+        previewLink: "https://github.com/rmarchino/digital-frontend.git"
     },
     {
         id: 3,
+        title: 'GuitarLA',
+        image: guitarra,
+        codeLink: "https://github.com/rmarchino/guitarLA.git",
+        previewLink: "https://lovely-jalebi-94d472.netlify.app/"
+    },
+    {
+        id: 4,
         title: 'Administrar productos',
         image: image1,
         codeLink: "https://github.com/rsteyker/administrar-products-react",
@@ -33,56 +42,56 @@ export const Data = [
     },
 
     {
-        id: 4,
+        id: 5,
         title: 'Aplicación Clima',
         image: image2,
         codeLink: "https://github.com/rsteyker/app-clima-react",
         previewLink: "https://aplication-clima-react.netlify.app/"
     },
     {
-        id: 5,
+        id: 6,
         title: 'Tú casa en el bosque',
         image: image3,
         codeLink: "https://github.com/rsteyker/Tu-casa-en-el-bosque",
         previewLink: "https://house-in-forest-website-aplication.netlify.app/"
     },
     {
-        id: 6,
+        id: 7,
         title: 'Cotiza criptomonedas al instante',
         image: image4,
         codeLink: "https://github.com/rsteyker/criptos-react",
         previewLink: "https://cotizar-criptomonedas-react-hook.netlify.app/"
     },
     {
-        id: 7,
+        id: 8,
         title: 'E-Commerce',
         image: image5,
         codeLink: "https://github.com/rsteyker/e-comerce",
         previewLink: "https://app.netlify.com/sites/e-comerce-react-redux/overview"
     },
     {
-        id: 8,
+        id: 9,
         title: 'Unete a la vida este es un meeti',
         image: image6,
         codeLink: "https://github.com/rsteyker/meeti",
         previewLink: "https://meeti-web-aplication.netlify.app/"
     },
     {
-        id: 9,
+        id: 10,
         title: 'Planificador de gastos',
         image: image7,
         codeLink: "https://github.com/rsteyker/administrar-gastos",
         previewLink: "https://administrar-tus-gastos-react.netlify.app/"
     },
     {
-        id: 10,
+        id: 11,
         title: 'Encuentra tu pokemon favorito',
         image: image8,
         codeLink: "https://github.com/rsteyker/pokedex-react-router",
         previewLink: "https://pokedex-rutas-protegidas-react.netlify.app/"
     },
     {
-        id: 11,
+        id: 12,
         title: 'Sonido profesional Sound Music',
         image: image9,
         codeLink: "https://github.com/rsteyker/soundmusic",
